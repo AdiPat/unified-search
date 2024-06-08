bd4b369503fa9f9012d377fad4a73d0d4128857d
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-09 01:06:06 +0530

    core/google-search: renaming and reduce manual timeouts

--- src/core/google-search.ts
+++ src/core/google-search.ts
@@ -6,7 +6,7 @@ import {
 } from "./search-engine";
 import { SEARCH_RESULTS_SCROLL_COUNTER } from "./constants";
 
-class GoogleSearchEngine implements SearchEngine {
+class GoogleSearch implements SearchEngine {
   private instance: Browser | null; // browser instance
   private curPage: Page | null;
   private isSearchPage: boolean = false;
@@ -124,7 +124,10 @@ class GoogleSearchEngine implements SearchEngine {
 
     let scrollCounter = 0;
 
-    while (!spanWithText && scrollCounter++ < SEARCH_RESULTS_SCROLL_COUNTER) {
+    while (
+      !(spanWithText || spanWithText.trim() == "") ||
+      scrollCounter++ < SEARCH_RESULTS_SCROLL_COUNTER
+    ) {
       paginationFound = await this.curPage.$$eval("tbody", (tables) => {
         return tables.length > 0;
       });
@@ -133,7 +136,7 @@ class GoogleSearchEngine implements SearchEngine {
         break;
       }
 
-      await new Promise((res) => setTimeout(res, 1000));
+      await new Promise((res) => setTimeout(res, 200));
       await this.curPage.evaluate(() => {
         window.scrollTo(0, document.body.scrollHeight);
       });
@@ -166,4 +169,4 @@ class GoogleSearchEngine implements SearchEngine {
   }
 }
 
-export { GoogleSearchEngine };
+export { GoogleSearch };
