ef6386492a445e059ce7deab927e5b4a8ca7b01e
Aditya Patange <contact.adityapatange@gmail.com>
2024-06-09 02:57:53 +0530

    add typings

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export { GoogleSearch, BingSearch, DuckDuckGoSearch } from "./core";
+export {
+  SearchEngine,
+  SearchEngineType,
+  SearchResult,
+  SearchQueryResult,
+} from "./core/search-engine";
